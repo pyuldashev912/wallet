421cd99adbf77ba9f939d6d35c015330ac67880b
pyuldashev912 <www.parviz98@ya.ru>
2023-01-23 11:45:32 +0500

    FEAT: Added payment reject functionality

--- pkg/wallet/wallet.go
+++ pkg/wallet/wallet.go
@@ -8,10 +8,12 @@ import (
 
 var (
 	ErrAccountNotFound = errors.New("account not found")
+	ErrPaymentNotFound = errors.New("payment not found")
 )
 
 type Service struct {
 	accounts []*types.Account
+	payments []*types.Payment
 }
 
 // FindAccountById возвращает указатель на найденный аккаунт
@@ -23,4 +25,31 @@ func (s *Service) FindAccountById(accountId int64) (*types.Account, error) {
 	}
 
 	return nil, ErrAccountNotFound
+}
+
+// Reject ...
+func (s *Service) Reject(paymentID string) error {
+	payment, err := s.FindPaymentById(paymentID)
+	if err != nil {
+		return err
+	}
+
+	payment.Status = types.StatusFail
+	// Пропускаем проверку на ошибку, так как платеж не может быть совершен
+	// с несуществующего аккаунта
+	account, _ := s.FindAccountById(payment.AccountID)
+	account.Balance += payment.Amount
+
+	return nil
+}
+
+// FindPaymentById
+func (s *Service) FindPaymentById(paymentID string) (*types.Payment, error) {
+	for _, currPayment := range s.payments {
+		if currPayment.ID == paymentID {
+			return currPayment, nil
+		}
+	}
+
+	return nil, ErrPaymentNotFound
 }
